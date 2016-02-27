f992e1ae3a7b9e89783454f91cec4d0c37c6a80e
Mansur Mabo <mansurmabo@gmail.com>
2016-02-27 23:30:13 +0600

    Created left image-links container
    added friendly resources links

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $(".owl-carousel").owlCarousel({
-        autoPlay: 5000, //Set AutoPlay to 3 seconds
+        autoPlay: 5000,
+        itemsScaleUp: false,
         items : 4,
         itemsDesktop : [1199,3],
         itemsDesktopSmall : [979,3]
