4029e57794f5cb5273967c2bb09c34b2197de488
Mansur Mabo <mansurmabo@gmail.com>
2016-02-29 00:40:16 +0600

    Created articles types Added menu wtih links to types of articles on news page now we can click on any type of news and it's change dynamically change styles of left images menu(align center) change routes to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
   def index
+    @types = Type.all
     @articles = Article.order(created_at: :desc)
   end
+
+  def get_news
+    @type = Type.find(params[:id])
+    @articles = Article.where(type_id: @type)
+  end
+
   def show
     @article = Article.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
   root 'articles#index'
+  get 'get_news/:id' => 'articles#get_news', as: :get_news
   resources :articles
   resources :pages
   resources :categories
