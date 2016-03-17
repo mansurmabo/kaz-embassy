cf7f58c39dccc33b1240daa4da6bb5035fef08e1
Mansur Mabo <mansurmabo@gmail.com>
2016-03-18 02:07:31 +0600

    Changed kaminari view added link to news on navbar menu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
   def index
     @types = Type.all
-    @articles = Article.order(created_at: :desc).page(params[:page]).per(20)
+    @articles = Article.order(created_at: :desc).page(params[:page]).per(2)
   end
 
   def get_news
     @type = Type.find(params[:id])
-    @articles = Article.where(type_id: @type).order(created_at: :desc).page(params[:page]).per(20)
+    @articles = Article.where(type_id: @type).order(created_at: :desc).page(params[:page]).per(2)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :galleries
   mount Ckeditor::Engine => '/ckeditor'
   root 'articles#index'
   get 'get_news/:id' => 'articles#get_news', as: :get_news
   resources :articles
   resources :pages
   resources :categories
+  resources :galleries
 
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
