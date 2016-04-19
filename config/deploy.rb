44b6d17c6765eed77fda21d88cd8100c00c543d5
Mansur Mabo <mansurmabo@gmail.com>
2016-04-20 01:42:26 +0600

    Fix nginx conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,12 @@
-server '176.126.167.232', port: 80, roles: [:web, :app, :db], primary: true
+server '176.126.167.232', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:mansurmabo/kaz-embassy.git'
 set :application,     'kaz-embassy'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+
+set :assets_roles, [:web, :app]
 
 # Don't change these unless you know what you're doing
 set :pty,             true
