57631427e0697efa6055edbc1455c89d0520ab10
Mansur <mansurmabo@gmail.com>
2016-05-18 03:31:02 +0600

    Add en ru locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
 
   def access_denied(exception)
     redirect_to admin_root_path, :alert => exception.message
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+  def default_url_options(option = {})
+    {locale: I18n.locale}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'images_categories/index'
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'images_categories/index'
+    mount Ckeditor::Engine => '/ckeditor'
+    root 'articles#index'
+    get 'get_news/:id' => 'articles#get_news', as: :get_news
+    get 'galleries/get_category/:id' => 'galleries#get_category', as: :get_categories
+    get 'search', to: 'search#search'
+    resources :articles
+    resources :pages, only: [:index, :show]
+    resources :categories, only: [:index]
+    resources :events, only: [:index]
+    resources :galleries, only: [:index, :show]
 
-  mount Ckeditor::Engine => '/ckeditor'
-  root 'articles#index'
-  get 'get_news/:id' => 'articles#get_news', as: :get_news
-  get 'galleries/get_category/:id' => 'galleries#get_category', as: :get_categories
-  get 'search', to: 'search#search'
-  resources :articles
-  resources :pages, only: [:index, :show]
-  resources :categories, only: [:index]
-  resources :events, only: [:index]
-  resources :galleries, only: [:index, :show]
-
+  end
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+#  match '*path', to: redirect("/#{I18n.default_locale}/%{path}")
+ # match '*', to: redirect("/#{I18n.default_locale}")
 end
