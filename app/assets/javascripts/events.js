f5236851dec4e8c63aa055f1c780eeb3fd8489f2
Mansur <mansurmabo@gmail.com>
2016-06-11 03:15:17 +0600

    Fix events json

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     var lang = window.location.href.toString().split('/')[3];
-    if(lang == 'kz') {
+    if (lang == 'kz') {
         $('#calendar').fullCalendar({
             eventSources: [
                 {
-                    url: '/events.json'
+                    url: "events.json"
                 }
             ],
             header: {
@@ -12,32 +12,33 @@ $(document).ready(function () {
                 center: 'title',
                 right: 'next, nextYear'
             },
-            monthNames: ["Қаңтар","Ақпан","Наурыз","Сәуір","Мамыр","Маусым","Шілде", "Тамыз", "Қыркүйек", "Қазан", "Қараша", "Желтоқсан" ],
-            monthNamesShort: ['Қаң','Ақп','Нау','Сәу','Мам','Мау','Шіл','Там','Қыр','Қаз','Қар','Жел'],
-            dayNames: [ 'Дүйсенбі', 'Сейсенбі', 'Сәрсенбі', 'Бейсенбі', 'Жұма', 'Сенбі', 'Жексенбі'],
-            dayNamesShort: ['Дс','Сс','Ср','Бс','Жм','Сн','Жк'],
+            monthNames: ["Қаңтар", "Ақпан", "Наурыз", "Сәуір", "Мамыр", "Маусым", "Шілде", "Тамыз", "Қыркүйек", "Қазан", "Қараша", "Желтоқсан"],
+            monthNamesShort: ['Қаң', 'Ақп', 'Нау', 'Сәу', 'Мам', 'Мау', 'Шіл', 'Там', 'Қыр', 'Қаз', 'Қар', 'Жел'],
+            dayNames: ['Дүйсенбі', 'Сейсенбі', 'Сәрсенбі', 'Бейсенбі', 'Жұма', 'Сенбі', 'Жексенбі'],
+            dayNamesShort: ['Дс', 'Сс', 'Ср', 'Бс', 'Жм', 'Сн', 'Жк'],
             defaultView: 'month',
             editable: false,
             timezone: "UTC"
 
         });
-    }else{
-    $('#calendar').fullCalendar({
-        eventSources: [
-            {
-                url: '/events.json'
-            }
-        ],
-        header: {
-            left: 'prevYear, prev,',
-            center: 'title',
-            right: 'next, nextYear'
-        },
-        lang: lang,
-        defaultView: 'month',
-        editable: false,
-        timezone: "UTC"
+    } else {
+        $('#calendar').fullCalendar({
+            eventSources: [
+                {
+                    url: "events.json"
 
-    });
+                }
+            ],
+            header: {
+                left: 'prevYear, prev,',
+                center: 'title',
+                right: 'next, nextYear'
+            },
+            lang: lang,
+            defaultView: 'month',
+            editable: false,
+            timezone: "UTC"
+
+        });
     }
 });
