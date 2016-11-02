bf1ec7a396b9a2fc9235202222073e971726237f
Mansur <mansurmabo@gmail.com>
2016-11-02 14:47:28 +0600

    Add passport fields

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,4 +1,8 @@
 class Citizen < ActiveRecord::Base
   has_one :citizen_passport
   has_many :citizen_relations
+
+  accepts_nested_attributes_for :citizen_passport
+  accepts_nested_attributes_for :citizen_relations
+
 end
